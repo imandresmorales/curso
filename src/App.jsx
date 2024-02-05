b8c72af472df620e9503a5c2057439dea244da83
Andres Morales <moralesandres@outlook.com>
2024-02-05 16:27:21 -0500

    2.2 y 2.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
 const Course = ({course}) => {
   const arreglo = course.parts
+  let total= arreglo.reduce( (result, element) => {
+    return result + element.exercises;
+  },0);
 
   return(
     <>
       <h1>{course.name}</h1>
-      <ul>
-        {arreglo.map( part => 
-          <li key={part.id}>
+      {arreglo.map( part => 
+          <p key={part.id}>
             {part.name} {part.exercises}
-          </li>
-        )}
-      </ul>
+          </p>
+      )}
+      <strong>total of {total} exercises</strong>
     </>
   )
 }
