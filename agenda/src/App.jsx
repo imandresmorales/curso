9d45addcf578d4ab491469916b0ddf0d1acb6fdf
Andres Morales <moralesandres@outlook.com>
2024-02-08 10:02:16 -0500

    2.8 update

--- agenda/src/App.jsx
+++ agenda/src/App.jsx
@@ -2,27 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
-
-  const [phones, setPhones] = useState([
-    { phone: '040-1234567' }
-  ])
   const [newPhone, setNewPhone] = useState('')
-  const [arreglo, setArreglo] = useState([
-    {name: 'Arto Hellas', phone: '040-1234567'}
-  ])
 
   const addName = (event) => {
     event.preventDefault()
     let i=0
-    const nameObject = {
-      name: newName
-    }
-    const nameObject2 = {
-      phone: newPhone
-    }
 
     persons.map(obj =>{
       if(obj.name === newName){
@@ -32,16 +19,13 @@ const App = () => {
     })
 
     if(i === 0){
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setPhones(phones.concat(nameObject2))
-      setNewPhone('')
       const obj={
         name: newName, phone: newPhone
       }
-      setArreglo(arreglo.concat(obj))
+      setPersons(persons.concat(obj))
+      setNewName('')
+      setNewPhone('')
     }
-    
   }
 
   const handleNameChange = (event) => {
@@ -68,11 +52,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>       
       <div>
-        {arreglo.map(obj => 
+        {persons.map(obj => 
               <p key={obj.name}>{obj.name} {obj.phone} </p>
         )}
-      </div>
-          
+      </div>   
     </div>
   )
 }
